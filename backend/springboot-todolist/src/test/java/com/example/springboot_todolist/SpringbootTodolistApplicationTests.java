24cb07e09de6848e6715b55de4d7eb29a08c379c
t-mine <tomonori.mine.biz@gmail.com>
2025-10-08 20:07:16 +0900

    コメント追加

--- backend/springboot-todolist/src/test/java/com/example/springboot_todolist/SpringbootTodolistApplicationTests.java
+++ backend/springboot-todolist/src/test/java/com/example/springboot_todolist/SpringbootTodolistApplicationTests.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootTodolistApplicationTests {
 
+	/*
+	 * もしSpringアプリの設定にエラーがあれば、このテストが失敗する。
+	 * 例：Bean定義のミス、依存関係エラー、環境変数の不足など
+	 * 　　Beanとは、@Serviceなどで自動で作られるインスタンスのこと
+	 * 
+	 */
 	@Test
 	void contextLoads() {
 	}
